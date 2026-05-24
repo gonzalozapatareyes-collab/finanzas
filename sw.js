7a85db63bbce56b628d45a6d5228fc43edaafe48
gonzalozapatareyes-collab <gonzalozapatareyes@gmail.com>
2026-05-24 15:11:17 -0400

    Add files via upload
    v29 - fix depósito mensual + Total Ahorros + título Insights mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker v28 - Mis Finanzas con biometría
-const CACHE_NAME = 'mis-finanzas-v28';
+// Service Worker v29 - Mis Finanzas
+const CACHE_NAME = 'mis-finanzas-v29';
 const APP_FILES = [
   './',
   './index.html',
