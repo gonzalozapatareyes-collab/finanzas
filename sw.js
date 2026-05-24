dd460142b076c111c88ca627f7534fe6cc8f650d
gonzalozapatareyes-collab <gonzalozapatareyes@gmail.com>
2026-05-24 15:33:33 -0400

    Add files via upload
    v30 - patrimonio real visible en todas las vistas (incluye acumulado mensual)

--- sw.js
+++ sw.js
@@ -1,59 +1,32 @@
-// Service Worker v29 - Mis Finanzas
-const CACHE_NAME = 'mis-finanzas-v29';
+// Service Worker v30 - Mis Finanzas
+const CACHE_NAME = 'mis-finanzas-v30';
 const APP_FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './apple-touch-icon.png',
-  './favicon-32.png'
+  './', './index.html', './manifest.json',
+  './icon-192.png', './icon-512.png',
+  './apple-touch-icon.png', './favicon-32.png'
 ];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_FILES).catch((err) => console.warn('SW: error cacheando', err));
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES).catch(()=>{})));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cn) => {
-          if (cn !== CACHE_NAME) {
-            console.log('SW: eliminando cache viejo', cn);
-            return caches.delete(cn);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then((cn) => Promise.all(cn.map((c) => c !== CACHE_NAME ? caches.delete(c) : null))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   if (url.hostname !== self.location.hostname) return;
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response && response.status === 200) {
-          const responseClone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+    fetch(event.request).then((response) => {
+      if (response && response.status === 200) {
+        const c = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, c));
+      }
+      return response;
+    }).catch(() => caches.match(event.request).then((c) => c || caches.match('./index.html')))
   );
 });
-
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
